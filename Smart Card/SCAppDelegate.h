6d7a5632c1b03c741a07a942068ecbc44cade353
victtavares <victtavares1@gmail.com>
2014-03-13 14:47:24 -0500

    Initial Commit

---
+++ Smart Card/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  Smart Card
+//
+//  Created by Victor Tavares on 3/13/14.
+//  Copyright (c) 2014 CS407. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Smart Card/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Smart Card
+//
+//  Created by Victor Tavares on 3/13/14.
+//  Copyright (c) 2014 CS407. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+    }
+}
